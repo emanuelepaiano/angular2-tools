6db2307f05bc8dc53322429cc3604885e62f6e12
Emanuele Paiano <paiano.emanuele@gmail.com>
2018-05-17 12:09:46 +0200

    Create password.services.ts

---
+++ password.services.ts
@@ -0,0 +1,46 @@
+/**
+ * Random Password generator for Angular 2+
+ * 
+ * 2018 - Emanuele Paiano
+ * 
+ * 
+ */
+
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PasswordServices {
+    private MinLength = 8;
+    constructor() {}
+
+    generateRandom(length: number, uppercase: boolean, lowercase: boolean, symbols: boolean) {
+        length < this.MinLength ? length = this.MinLength : length = length;
+        let password;
+        uppercase ? password = this.generateChar(true, false, false) : password = '1';
+        for (let i = 0; i < length - 3 && (uppercase || lowercase) ; i++) {
+            password += this.generateChar(uppercase, lowercase, false);
+        }
+        lowercase ? password += this.generateChar(false, lowercase, false) : password = password;
+        symbols ? password += this.generateChar(false, false, true) : password += this.generateChar(uppercase, lowercase, false);
+        return password;
+    }
+
+    generateChar(uppercase: boolean, lowercase: boolean, symbols: boolean) {
+        const setChar: number[] = new Array;
+        if (uppercase) {
+            setChar[setChar.length] = this.generateBetween(65, 90);
+        }
+        if (lowercase) {
+            setChar[setChar.length] = this.generateBetween(97, 122);
+        }
+        if (symbols) {
+            setChar[setChar.length] = this.generateBetween(39, 47);
+            setChar[setChar.length] = this.generateBetween(58, 64);
+        }
+        return String.fromCharCode(setChar[this.generateBetween(0, setChar.length - 1)]);
+    }
+
+    private generateBetween(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
